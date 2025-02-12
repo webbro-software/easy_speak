c5628519bd345fb44cf64444e58b2453b74ea82b
Usmonov Shohruxmirzo <usmonovshohruxmirzo@gmail.com>
2025-02-12 21:29:34 +0500

    feat: setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='easy_speech',
+    version='0.1.0',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    install_requires=[],
+    author='Usmonov Shoxruxmirzo',
+    author_email='usmonovshohruxmirzo@gmail.com',
+    description='Effortless and powerful text-to-speech conversion for Python applications.' ,
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/webbro-software/gcd-calculator',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
